028ef6f3b28707c49ade3de8acb5b79d1847a550
Alexander Stromberger <A@ST.fyi>
2022-05-04 13:46:09 +0200

    initial

---
+++ app.jl
@@ -0,0 +1,117 @@
+using Random
+using GLMakie
+using Base.Threads
+using NearestNeighbors
+using StaticArrays
+using Profile
+#using ProfileView
+
+n = 100*500
+vmax = 0.03
+d0 = 0.02
+dc = 0.01
+l0 = 0.31
+l1 = 0.001
+l2 = 1.2
+l3 = 2
+l4 = 0.01
+
+struct Boid
+    position::SVector{3, Float64}
+    velocity::SVector{3, Float64}
+end
+
+norm(a1) = sqrt((a1[1])^2 + (a1[2])^2 + (a1[3])^2)
+distance(a1, a2) = sqrt((a1[1] - a2[1])^2 + (a1[2] - a2[2])^2 + (a1[3] - a2[3])^2)
+init_boid()::Boid = Boid(1.0 .- 2 * rand(Float64,3), 0.001 .* rand(Float64,3))
+
+# update function
+function update_boid(b, blist, tree)
+    nearby = first(knn(tree, b.position, 25))
+
+    n_length = 0
+    c_length = 0
+
+    s1 = zeros(SVector{3})
+    s2 = zeros(SVector{3})
+    s3 = zeros(SVector{3})
+    for i in 1:length(nearby)
+        nb = blist[nearby[i]]
+        d = distance(nb.position, b.position)
+        if d < d0
+            s1 += nb.position
+            s2 += nb.velocity
+            if d < dc
+                s3 += nb.position - b.position
+                c_length += 1
+            end
+            n_length += 1
+        end
+    end
+
+    w1 = (1 / n_length * s1) - b.position
+    w2 = (1 / n_length * s2)
+    w3 = (-1 / c_length * s3)
+    w4 = maximum(abs.(b.position)) <= 1 ? zeros(3) : -b.position
+
+    velocity = b.velocity * l0 + w1 * l1 + w2 * l2 + w3 * l3 + w4 * l4
+
+    vnorm = norm(velocity)
+    if vnorm > vmax
+        velocity = vmax * (velocity / vnorm)
+    end
+
+    position = b.position + velocity
+    Boid(position, velocity)
+end
+
+
+locations(boids::Vector{Boid}) = [x.position for x in boids]
+
+# init
+boids = [init_boid() for _ in 1:n]
+kdtree = KDTree(locations(boids); leafsize = 10)
+
+fig = Figure()
+ax3d = Axis3(fig[1, 1]; aspect = (1, 1, 1),
+    perspectiveness = 0.3, azimuth = 0.8, elevation = 0.30, show_axis = false)
+#hidedecorations!(ax3d)
+#hidespines!(ax3d)
+points = Observable([Point3f(x.position[1], x.position[2], x.position[3]) for x in boids])
+scatter!(ax3d,points,markersize=10)
+display(fig)
+
+
+buffer = copy(boids)
+
+function update_sim()
+    Threads.@threads for i = 1:length(buffer)
+        buffer[i] = update_boid(buffer[i], boids, kdtree)
+    end
+    global boids = buffer
+    # updated indices
+    global kdtree = KDTree(locations(boids); leafsize = 10, reorder = true)
+end
+
+(for t in 1:2000
+    println(t)
+    update_sim()
+    points[] = [Point3f(x.position[1], x.position[2], x.position[3]) for x in boids]
+    #sleep(0.0001)
+end)
+
+
+
+"""record(fig, "append_animation.mp4", 1:30*20;
+        framerate = 30) do frame
+            println(frame)
+            Threads.@threads for i = 1:length(buffer)
+                buffer[i] = update_boid(buffer[i], boids, kdtree)
+            end
+            global boids = buffer
+            points[] = [Point3f(x.position[1], x.position[2], x.position[3]) for x in boids]
+            # updated indices
+            global kdtree = KDTree(locations(boids); leafsize = 10)
+        
+            sleep(0.0001)
+end"""
---
+++ app.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+from functools import reduce
+from rtree import index
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+n = 100
+vmax = 0.03
+do = 0.02
+dc = 0.01
+l0 = 0.31
+l1 = 0.001
+l2 = 1.2
+l3 = 2
+l4 = 0.01
+
+
+@dataclass
+class Boid:
+    position: np.ndarray = np.random.random(3)
+    velocity: np.ndarray = np.array([0.0, 0.01, 0.01])
+
+
+def distance(p1, p2) -> float:
+    return np.linalg.norm(p1 - p2, 2)
+
+
+# init
+p = index.Property()
+p.dimension = 3
+idx = index.Index(properties=p)
+boids = [Boid(np.random.random(3), np.random.random(3) / 100) for _ in range(n + 1)]
+
+for i, b in enumerate(boids):
+    idx.add(i,b.position.tolist())
+
+
+def update(boid: Boid, other_b: [Boid], idx):
+    hacky_other_b = [boids[bid] for bid in idx.nearest(boid.position.tolist(), 10)]
+    others = [b for b in hacky_other_b if distance(b.position, boid.position) < do]
+    others_close = [b for b in others if distance(b.position, boid.position) < dc]
+    others_len = len(others)
+    others_close_len = len(others_close)
+
+    w1 = (1 / others_len * reduce(lambda x, y: x + y, [x.position for x in others])) - boid.position
+    w2 = (1 / others_len * reduce(lambda x, y: x + y, [x.velocity for x in others]))
+    w3 = (1 / others_close_len * reduce(lambda x, y: x + y, [o.position - boid.position for o in others_close]))
+    w4 = np.zeros(3) if np.max(np.abs(boid.position)) <= 1 else -boid.position
+
+    velocity = boid.velocity * l0 + w1 * l1 + w2 * l2 + w3 * l3 + w4 * l4
+
+    if np.linalg.norm(velocity) > vmax:
+        velocity = vmax * (velocity / np.linalg.norm(velocity))
+
+    position = boid.position + velocity
+    return Boid(position, velocity)
+
+"""
+for t in range(20):
+    print(t)
+    boids = [update(b, boids, idx) for b in boids]
+    for i, b in enumerate(boids):
+        idx.add(i,b.position.tolist())
+"""
+
+
+#boids = [Boid(np.random.random(3), np.random.random(3) / 100) for _ in range(n + 1)]
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlim3d(-1, 1)
+ax.set_ylim3d(-1, 1)
+ax.set_zlim3d(-1, 1)
+graph = ax.scatter([x.position[0] for x in boids], [x.position[1] for x in boids], [x.position[2] for x in boids])
+
+
+
+def update_plt(num):
+    print(num)
+    global boids
+    boids = [update(b, boids,idx) for b in boids]
+    graph._offsets3d = (np.array([x.position[0] for x in boids]), np.array([x.position[1] for x in boids]),
+                        np.array([x.position[2] for x in boids]))
+
+    for i, b in enumerate(boids):
+        idx.add(i,b.position.tolist())
+
+    return graph,
+
+
+ani = animation.FuncAnimation(fig, update_plt, frames=2000, blit=True)
+ani.save("test.mp4", fps=30)
